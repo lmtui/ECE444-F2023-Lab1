19f8901e95d40805a0b1d5c144caf288c7e2f302
Lisa Mtui <lisamtui@gmail.com>
2023-09-24 22:51:53 -0400

    updated hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,23 @@
-from flask import Flask
+
+from flask import Flask, render_template
+from datetime import datetime
+
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    current_time = datetime.utcnow()  # Get the current time
+    return render_template('index.html', current_time=current_time)
 
 @app.route('/user/<name>')
 def user(name):
-    return  '<h1>Hello, {}!</h1>'.format(name)
+    print(datetime.utcnow())
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
+
+if __name__ == '__main__':
+    app.run(debug=True)
